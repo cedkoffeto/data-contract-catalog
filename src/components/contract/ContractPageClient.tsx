00bd45b1b874f912bc44469129ffd6bc9b770ee9
Mouhamadou Al Moustapha DIOP <mouhamadou.al.moustapha.pro@gmail.com>
2026-06-23 18:06:31 +0100

    ExportButton: use flex justify-center instead of self-center (not in compiled CSS)

--- src/components/contract/ContractPageClient.tsx
+++ src/components/contract/ContractPageClient.tsx
@@ -50,7 +50,7 @@ function ExportButton({ slug }: { slug: string }) {
   }
 
   return (
-    <div ref={ref} className="relative self-center w-fit">
+    <div ref={ref} className="relative flex justify-center">
       <button
         type="button"
         onClick={() => setOpen((v) => !v)}
