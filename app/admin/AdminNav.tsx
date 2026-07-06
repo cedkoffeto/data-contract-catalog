cf836c955062051e9a995d9b7b208d4fe14c6e17
Mouhamadou Al Moustapha DIOP <mouhamadou.al.moustapha.pro@gmail.com>
2026-07-06 12:54:03 +0100

    refactor: rename Dashboard to Administration with home icon

--- app/admin/AdminNav.tsx
+++ app/admin/AdminNav.tsx
@@ -5,8 +5,8 @@ import { usePathname } from "next/navigation";
 const links = [
   {
     href: "/admin",
-    label: "Dashboard",
-    icon: "M3 4a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zm7-1a1 1 0 00-1 1v2a1 1 0 001 1h2a1 1 0 001-1V4a1 1 0 00-1-1h-2zm7 0a1 1 0 011 1v2a1 1 0 01-1 1h-2a1 1 0 01-1-1V4a1 1 0 011-1h2zM3 12a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1v-2zm7-1a1 1 0 00-1 1v2a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 00-1-1h-2zm7 0a1 1 0 011 1v2a1 1 0 01-1 1h-2a1 1 0 01-1-1v-2a1 1 0 011-1h2z",
+    label: "Administration",
+    icon: "M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z",
   },
   {
     href: "/admin/groups",
