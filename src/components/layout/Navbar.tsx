a8be708cd18236bac376a0e7d893ce07181dfe4e
Mouhamadou Al Moustapha DIOP <mouhamadou.al.moustapha.pro@gmail.com>
2026-07-08 10:07:40 +0100

    revert: restore original Navbar without MobileMenu (server/client prop serialization issue)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { t } from "@/src/lib/i18n";
 import { auth } from "@/src/auth";
 import { AdminMenu } from "@/src/components/layout/AdminMenu";
-import { MobileMenu } from "@/src/components/layout/MobileMenu";
 import { NotificationBell } from "@/src/components/layout/NotificationBell";
 import { UserMenu } from "@/src/components/layout/UserMenu";
 import { getUserPermissions } from "@/src/lib/rbac";
@@ -35,52 +34,38 @@ export async function Navbar() {
           </Link>
 
           <div className="site-nav__actions">
-            <div className="hidden md:flex md:items-center md:gap-3">
-              <div className="site-nav__links">
-                <Link className="site-nav__link" href="/">
-                  <svg className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zm0 6a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1v-2zm0 6a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1v-2z" />
-                  </svg>
-                  {t("catalogTitle")}
-                </Link>
-                <Link className="site-nav__link" href="/docs">
-                  <svg className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm2.25 8.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 3a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z" clipRule="evenodd" />
-                  </svg>
-                  Api
-                </Link>
-                <Link className="site-nav__link" href="/data-model">
-                  <svg className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path d="M10 3C5.58 3 2 4.12 2 5.5v9c0 1.38 3.58 2.5 8 2.5s8-1.12 8-2.5v-9C18 4.12 14.42 3 10 3zm0 2c3.87 0 6 .87 6 1.5S13.87 8 10 8s-6-.87-6-1.5S6.13 5 10 5zm6 9.5c0 .63-2.13 1.5-6 1.5s-6-.87-6-1.5v-2.23c1.42.85 3.58 1.23 6 1.23s4.58-.38 6-1.23V14.5zm0-4c0 .63-2.13 1.5-6 1.5s-6-.87-6-1.5V8.27c1.42.85 3.58 1.23 6 1.23s4.58-.38 6-1.23V10.5z" />
-                  </svg>
-                  Data Model
-                </Link>
-                {isAdmin && <AdminMenu />}
-              </div>
-
-              <Link className="site-nav__cta" href="/editor">
+            <div className="site-nav__links">
+              <Link className="site-nav__link" href="/">
                 <svg className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path d="M13.586 2.586a2 2 0 012.828 0l.828.828a2 2 0 010 2.828l-9.172 9.172a2 2 0 01-1.068.566l-3.11.518a1 1 0 01-1.112-1.112l.518-3.11a2 2 0 01.566-1.068l9.172-9.172zM15.414 4.414a.5.5 0 00-.707 0l-1.06 1.06 1.768 1.768 1.06-1.06a.5.5 0 000-.707l-.828-.828z" />
+                  <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zm0 6a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1v-2zm0 6a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1v-2z" />
                 </svg>
-                Editor
+                {t("catalogTitle")}
               </Link>
-
-              {session ? <NotificationBell /> : null}
-
-              {session ? <UserMenu email={session.user?.email} image={session.user?.image} name={session.user?.name ?? displayName} givenName={(session.user as Record<string, unknown>)?.givenName as string | undefined} familyName={(session.user as Record<string, unknown>)?.familyName as string | undefined} userId={session.user?.name ?? undefined} /> : null}
+              <Link className="site-nav__link" href="/docs">
+                <svg className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fillRule="evenodd" d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm2.25 8.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 3a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z" clipRule="evenodd" />
+                </svg>
+                Api
+              </Link>
+              <Link className="site-nav__link" href="/data-model">
+                <svg className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path d="M10 3C5.58 3 2 4.12 2 5.5v9c0 1.38 3.58 2.5 8 2.5s8-1.12 8-2.5v-9C18 4.12 14.42 3 10 3zm0 2c3.87 0 6 .87 6 1.5S13.87 8 10 8s-6-.87-6-1.5S6.13 5 10 5zm6 9.5c0 .63-2.13 1.5-6 1.5s-6-.87-6-1.5v-2.23c1.42.85 3.58 1.23 6 1.23s4.58-.38 6-1.23V14.5zm0-4c0 .63-2.13 1.5-6 1.5s-6-.87-6-1.5V8.27c1.42.85 3.58 1.23 6 1.23s4.58-.38 6-1.23V10.5z" />
+                </svg>
+                Data Model
+              </Link>
+              {isAdmin && <AdminMenu />}
             </div>
 
-            <MobileMenu>
-              <div className="flex flex-col gap-1">
-                <Link className="site-nav__link" href="/" onClick={() => {}}>
-                  {t("catalogTitle")}
-                </Link>
-                <Link className="site-nav__link" href="/docs">Api</Link>
-                <Link className="site-nav__link" href="/data-model">Data Model</Link>
-                {isAdmin && <AdminMenu />}
-                <Link className="site-nav__cta mt-2" href="/editor">Editor</Link>
-              </div>
-            </MobileMenu>
+            <Link className="site-nav__cta" href="/editor">
+              <svg className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path d="M13.586 2.586a2 2 0 012.828 0l.828.828a2 2 0 010 2.828l-9.172 9.172a2 2 0 01-1.068.566l-3.11.518a1 1 0 01-1.112-1.112l.518-3.11a2 2 0 01.566-1.068l9.172-9.172zM15.414 4.414a.5.5 0 00-.707 0l-1.06 1.06 1.768 1.768 1.06-1.06a.5.5 0 000-.707l-.828-.828z" />
+              </svg>
+              Editor
+            </Link>
+
+            {session ? <NotificationBell /> : null}
+
+            {session ? <UserMenu email={session.user?.email} image={session.user?.image} name={session.user?.name ?? displayName} givenName={(session.user as Record<string, unknown>)?.givenName as string | undefined} familyName={(session.user as Record<string, unknown>)?.familyName as string | undefined} userId={session.user?.name ?? undefined} /> : null}
           </div>
         </div>
       </div>
