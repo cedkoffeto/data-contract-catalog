6f487922ed30d1c9820b0732407da36845235ae6
Mouhamadou Al Moustapha DIOP <mouhamadou.al.moustapha.pro@gmail.com>
2026-07-25 01:20:26 +0100

    Revert "fix: remove empty space at top of workspace, flatten page wrappers"
    This reverts commit f40d8f936a7988f2a741faeb5a1513464c1acf4b.

--- app/data-model/page.tsx
+++ app/data-model/page.tsx
@@ -18,12 +18,14 @@ export default async function DataModelPage(props: {
     <>
       <style>{`html { overflow: hidden !important; }`}</style>
       <PageShell showFooter={false} scrollable={false}>
-        <div className="relative min-h-0 flex-1 overflow-hidden">
-          <DataModelClient
-            focusSlug={focusSlug}
-            focusDomain={focusDomain}
-            focusContext={focusContext}
-          />
+        <div className="flex flex-1 flex-col overflow-hidden">
+          <div className="relative min-h-0 flex-1">
+            <DataModelClient
+              focusSlug={focusSlug}
+              focusDomain={focusDomain}
+              focusContext={focusContext}
+            />
+          </div>
         </div>
       </PageShell>
     </>
